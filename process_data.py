669fb45478f6c1331ef21c37132019f629cc980e
Vadim Berezniker <vadim@berezniker.com>
2015-03-22 18:17:51 -0400

    Refactor street name cleaning rules into classes.

--- process_data.py
+++ process_data.py
@@ -8,69 +8,91 @@ import pprint
 # Some street names are intersections "x and y", "x & y" or "x between y"
 # Some street names are duplicated "abc; xyz"
 
-street_type_re = re.compile(r'^(.*?)\b(\S+\.?)$', re.IGNORECASE)
-
-ALPHABET_AVE_RE = re.compile('^Avenue [A-Z]$')
-
-STEET_SUFFIX_ALTERNATIVES = {
-    'Alley': {},
-    'Americas': {},  # Avenue of the Americas
-    'Avenue': {'AVE.', 'AVENUE', 'Ave', 'Ave,', 'Ave.', 'Avenue,', 'ave', 'avenue', 'Avene', 'Aveneu'},
-    'Boulevard': {'Blv.', 'Blvd', 'Blvd.', 'boulevard'},
-    'Broadway': {},
-    'Center': {},
-    'Circle': {'Cir', 'CIRCLE'},
-    'Concourse': {},
-    'Court': {'Ct', 'Ct.'},
-    'Crescent': {},
-    'Drive': {'DRIVE', 'Dr', 'Dr.', 'drive'},
-    'East': {'EAST'},
-    'Expressway': {},
-    'Extension': {},
-    'Finest': {},  # Avenue of the finest
-    'Heights': {},
-    'Lane': {'LANE'},
-    'Loop': {},
-    'North': {'north'},
-    'Park': {},
-    'Parkway': {'Pkwy', 'Pky', 'PARKWAY'},
-    'Place': {'Pl', 'PLACE'},
-    'Plaza': {'Plz', 'PLAZA'},
-    'Road': {'ROAD', 'Rd', 'road'},
-    'Square': {},
-    'South': {},
-    'Street': {'St', 'St.', 'Steet', 'Street', 'st', 'street', 'STREET', 'ST'},
-    'Terrace': {},
-    'Turnpike': {'Tpke'},
-    'Walk': {},
-    'West': {'WEST'},
-    'Way': {'WAY'},
-}
+STREET_TYPE_RE = re.compile(r'^(.*?)\b(\S+\.?)$', re.IGNORECASE)
 
 
-def build_street_canonical_suffix_map():
-    """
-    Builds a map of street suffixes to their canonical representations.
-    """
-    canonical_suffix_map = {}
-    for canonical_suffix, alternative_suffixes in STEET_SUFFIX_ALTERNATIVES.iteritems():
-        # Map canonical suffix to itself for convenience.
-        canonical_suffix_map[canonical_suffix] = canonical_suffix
-        for alternative_suffix in alternative_suffixes:
-            canonical_suffix_map[alternative_suffix] = canonical_suffix
-    return canonical_suffix_map
+class StreetNameCanonicalSuffixCleaner(object):
+    _STREET_SUFFIX_ALTERNATIVES = {
+        'Alley': {},
+        'Americas': {},  # Avenue of the Americas
+        'Avenue': {'AVE.', 'AVENUE', 'Ave', 'Ave,', 'Ave.', 'Avenue,', 'ave', 'avenue', 'Avene', 'Aveneu'},
+        'Boulevard': {'Blv.', 'Blvd', 'Blvd.', 'boulevard'},
+        'Broadway': {},
+        'Center': {},
+        'Circle': {'Cir', 'CIRCLE'},
+        'Concourse': {},
+        'Court': {'Ct', 'Ct.'},
+        'Crescent': {},
+        'Drive': {'DRIVE', 'Dr', 'Dr.', 'drive'},
+        'East': {'EAST'},
+        'Expressway': {},
+        'Extension': {},
+        'Finest': {},  # Avenue of the finest
+        'Heights': {},
+        'Lane': {'LANE'},
+        'Loop': {},
+        'North': {'north'},
+        'Park': {},
+        'Parkway': {'Pkwy', 'Pky', 'PARKWAY'},
+        'Place': {'Pl', 'PLACE'},
+        'Plaza': {'Plz', 'PLAZA'},
+        'Road': {'ROAD', 'Rd', 'road'},
+        'Square': {},
+        'South': {},
+        'Street': {'St', 'St.', 'Steet', 'Street', 'st', 'street', 'STREET', 'ST'},
+        'Terrace': {},
+        'Turnpike': {'Tpke'},
+        'Walk': {},
+        'West': {'WEST'},
+        'Way': {'WAY'},
+        }
+
+    def build_street_canonical_suffix_map(self):
+        """
+        Builds a map of street suffixes to their canonical representations.
+        """
+        canonical_suffix_map = {}
+        for canonical_suffix, alternative_suffixes in self._STREET_SUFFIX_ALTERNATIVES.iteritems():
+            # Map canonical suffix to itself for convenience.
+            canonical_suffix_map[canonical_suffix] = canonical_suffix
+            for alternative_suffix in alternative_suffixes:
+                canonical_suffix_map[alternative_suffix] = canonical_suffix
+        return canonical_suffix_map
 
 
-def audit_street_type(street_types, street_name, street_canonical_suffix_map):
-    # Ignore letter avenues in Manhattan/Brooklyn.
-    if ALPHABET_AVE_RE.search(street_name):
-        return
+    def __init__(self):
+        self._canonical_suffix_map = self.build_street_canonical_suffix_map()
 
-    m = street_type_re.search(street_name)
+    def clean(self, street_name):
+        m = STREET_TYPE_RE.search(street_name)
+        if m:
+            street_name_prefix = m.group(1)
+            street_type = m.group(2)
+            if street_type in self._canonical_suffix_map:
+                return "%s%s" % (street_name_prefix, self._canonical_suffix_map[street_type])
+
+
+class StreetNameAvenueXCleaner(object):
+    _ALPHABET_AVE_RE = re.compile('^Avenue ([A-Z])$', re.IGNORECASE)
+
+    def clean(self, street_name):
+        m = self._ALPHABET_AVE_RE.search(street_name)
+        if m:
+            letter = m.group(1)
+            return 'Avenue %s' % letter
+
+
+def audit_street_type(street_types, street_name, cleaners):
+    for cleaner in cleaners:
+        res = cleaner.clean(street_name)
+        if res is not None:
+            print("[%s] => [%s]" % (street_name, res))
+            return
+
+    m = STREET_TYPE_RE.search(street_name)
     if m:
         street_type = m.group(2)
-        if street_type not in street_canonical_suffix_map:
-            street_types[street_type].add(street_name)
+        street_types[street_type].add(street_name)
 
 
 def is_street_name(elem):
@@ -78,7 +100,8 @@ def is_street_name(elem):
 
 
 def audit(osmfile):
-    street_canonical_suffix_map = build_street_canonical_suffix_map()
+    street_name_cleaners = [StreetNameCanonicalSuffixCleaner(), StreetNameAvenueXCleaner()]
+
     osm_file = open(osmfile, 'r')
     street_types = defaultdict(set)
     for event, elem in ElementTree.iterparse(osm_file, events=('start',)):
@@ -86,13 +109,13 @@ def audit(osmfile):
         if elem.tag == 'node' or elem.tag == 'way':
             for tag in elem.iter('tag'):
                 if is_street_name(tag):
-                    audit_street_type(street_types, tag.attrib['v'], street_canonical_suffix_map)
+                    audit_street_type(street_types, tag.attrib['v'], street_name_cleaners)
 
     return street_types
 
 
 def update_name(name, mapping):
-    m = street_type_re.search(name)
+    m = STREET_TYPE_RE.search(name)
     if m:
         street_name = m.group(1)
         street_type = m.group(2)
@@ -104,6 +127,9 @@ def update_name(name, mapping):
 
 if __name__ == '__main__':
     st_types = audit('new-york_new-york.osm')
+    print("")
+    print("")
+    print("Unclassified streets:")
     pprint.pprint(dict(st_types))
 
     # for st_type, ways in st_types.iteritems():
