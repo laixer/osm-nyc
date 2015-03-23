cfd4adddbe63c2424e2dcee2e5eaece95a947bec
Vadim Berezniker <vadim@berezniker.com>
2015-03-22 20:48:34 -0400

    Export data as JSON.

--- process_data.py
+++ process_data.py
@@ -2,11 +2,19 @@ import xml.etree.cElementTree as ElementTree
 from collections import defaultdict
 import re
 import pprint
+import codecs
+import json
 
 # Audit street names -> fix suffixes
 # Some street names include suite/apartment numbers (e.g. 2B, Suite 500)
 # Some street names are intersections "x and y", "x & y" or "x between y"
 # Some street names are duplicated "abc; xyz"
+
+lower = re.compile(r'^([a-z]|_)*$')
+lower_colon = re.compile(r'^([a-z]|_)*:([a-z]|_)*$')
+problemchars = re.compile(r'[=\+/&<>;\'"\?%#$@\,\. \t\r\n]')
+
+CREATED = ['version', "changeset", 'timestamp', 'user', 'uid']
 
 STREET_TYPE_RE = re.compile(r'^(.*?)\b(\S+\.?)$', re.IGNORECASE)
 
@@ -46,7 +54,7 @@ class StreetNameCanonicalSuffixCleaner(object):
         'Walk': {},
         'West': {'WEST'},
         'Way': {'WAY'},
-        }
+    }
 
     def build_street_canonical_suffix_map(self):
         """
@@ -60,7 +68,6 @@ class StreetNameCanonicalSuffixCleaner(object):
                 canonical_suffix_map[alternative_suffix] = canonical_suffix
         return canonical_suffix_map
 
-
     def __init__(self):
         self._canonical_suffix_map = self.build_street_canonical_suffix_map()
 
@@ -70,7 +77,7 @@ class StreetNameCanonicalSuffixCleaner(object):
             street_name_prefix = m.group(1)
             street_type = m.group(2)
             if street_type in self._canonical_suffix_map:
-                return "%s%s" % (street_name_prefix, self._canonical_suffix_map[street_type])
+                return '%s%s' % (street_name_prefix, self._canonical_suffix_map[street_type])
 
 
 class StreetNameAvenueXCleaner(object):
@@ -83,57 +90,71 @@ class StreetNameAvenueXCleaner(object):
             return 'Avenue %s' % letter
 
 
-def audit_street_type(street_types, street_name, cleaners):
+def clean_street_type(street_types, street_name, cleaners):
     for cleaner in cleaners:
         res = cleaner.clean(street_name)
         if res is not None:
-            print("[%s] => [%s]" % (street_name, res))
-            return
+            print('[%s] => [%s]' % (street_name, res))
+            return res
 
     m = STREET_TYPE_RE.search(street_name)
     if m:
         street_type = m.group(2)
         street_types[street_type].add(street_name)
+    return street_name
 
 
-def is_street_name(elem):
-    return elem.attrib['k'] == 'addr:street'
-
-
-def audit(osmfile):
-    street_name_cleaners = [StreetNameCanonicalSuffixCleaner(), StreetNameAvenueXCleaner()]
-
-    osm_file = open(osmfile, 'r')
+def process_map(file_in, pretty=False):
     street_types = defaultdict(set)
-    for event, elem in ElementTree.iterparse(osm_file, events=('start',)):
+    street_name_cleaners = [StreetNameCanonicalSuffixCleaner(), StreetNameAvenueXCleaner()]
+    file_out = '{0}.json'.format(file_in)
+    with codecs.open(file_out, 'w') as fo:
+        for _, element in ElementTree.iterparse(file_in):
+            el = shape_element(element)
+            if el:
+                if "address" in el and "street" in el["address"]:
+                    el["address"]["street"] = clean_street_type(
+                        street_types, el["address"]["street"], street_name_cleaners)
+                if pretty:
+                    fo.write(json.dumps(el, indent=2) + '\n')
+                else:
+                    fo.write(json.dumps(el) + '\n')
+    print('')
+    print('')
+    print('Unclassified streets:')
+    pprint.pprint(dict(street_types))
 
-        if elem.tag == 'node' or elem.tag == 'way':
-            for tag in elem.iter('tag'):
-                if is_street_name(tag):
-                    audit_street_type(street_types, tag.attrib['v'], street_name_cleaners)
 
-    return street_types
-
-
-def update_name(name, mapping):
-    m = STREET_TYPE_RE.search(name)
-    if m:
-        street_name = m.group(1)
-        street_type = m.group(2)
-        if street_type in mapping:
-            name = street_name + mapping[street_type]
-
-    return name
+def shape_element(element):
+    node = {}
+    if element.tag == 'node' or element.tag == 'way':
+        node['type'] = element.tag
+        node['created'] = {}
+        for k, v in element.attrib.iteritems():
+            if k in CREATED:
+                node['created'][k] = v
+            elif k != 'lat' and k != 'lon':
+                node[k] = v
+        if 'lat' in element.attrib and 'lon' in element.attrib:
+            node['pos'] = [float(element.attrib['lat']), float(element.attrib['lon'])]
+        for el in element.iter('tag'):
+            k = el.attrib['k']
+            v = el.attrib['v']
+            if not problemchars.search(k):
+                if k.startswith('addr:'):
+                    if 'address' not in node:
+                        node['address'] = {}
+                    nk = k[5:]
+                    if ':' not in nk:
+                        node['address'][nk] = v
+        for el in element.iter('nd'):
+            if 'node_refs' not in node:
+                node['node_refs'] = []
+            node['node_refs'].append(el.attrib['ref'])
+        return node
+    else:
+        return None
 
 
 if __name__ == '__main__':
-    st_types = audit('new-york_new-york.osm')
-    print("")
-    print("")
-    print("Unclassified streets:")
-    pprint.pprint(dict(st_types))
-
-    # for st_type, ways in st_types.iteritems():
-    # for name in ways:
-    # better_name = update_name(name, mapping)
-    #         print name, "=>", better_name
+    process_map('new-york_new-york.osm')
